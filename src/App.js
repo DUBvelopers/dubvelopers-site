140b5d456ead8cd251792ee68a58c99aead3fe65
mathlord2 <erixrekt69@gmail.com>
2022-02-14 22:11:28 -0800

    Initialized React App + libraries

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  NavLink,
+  Route
+} from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import Team from "./pages/Team/Team";
+
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <ul className="navbar">
+        <li>
+          <NavLink exact to="/" className="link">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/team" className="link">Our Team</NavLink>
+        </li>
+      </ul>
+        
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route path="/team" element={<Team/>}/>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Team/Team.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Team = () => {
+    return (
+        <div>
+            Hi Atharva :)
+        </div>
+    );
+}
+
+export default Team;
