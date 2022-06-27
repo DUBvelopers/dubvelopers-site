5c14c7fbda9f08700855f980a761501d83a49ac5
mathlord2 <erixrekt69@gmail.com>
2022-06-27 15:59:49 -0700

    Added resources for this year

--- src/pages/Resources/Resource.js
+++ src/pages/Resources/Resource.js
@@ -2,17 +2,31 @@ import React from "react";
 
 import "./Resources.css";
 
+const getDate = date => {
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+    return month + " " + day + ", " + year;
+}
+
 const Resource = props => {
     return (
         <div className="resource">
             <div className="text">
                 <h4>{props.resource.season} Week {props.resource.week} - {props.resource.title}</h4>
-                <p>{props.resource.date}</p>
+                <p>{getDate(props.resource.date)}</p>
             </div>
 
-            <a href={props.resource.link} target="_blank" rel="noreferrer">
-                <button>View</button>
-            </a>
+            <div className="buttons">
+                {props.resource.slidesLink && <a href={props.resource.slidesLink} target="_blank" rel="noreferrer">
+                    <button>Slides</button>
+                </a>}
+                {props.resource.videoLink && <a href={props.resource.videoLink} target="_blank" rel="noreferrer">
+                    <button>Video</button>
+                </a>}
+            </div>
         </div>
     );
 }
