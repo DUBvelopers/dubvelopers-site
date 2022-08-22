002198dffb284b9139f26a5eaf1b8db415a972f9
mathlord2 <erixrekt69@gmail.com>
2022-08-22 13:18:19 -0400

    Homepage + navbar now mobile-friendly

--- src/App.js
+++ src/App.js
@@ -11,29 +11,26 @@ import Project from "./pages/Project/Project";
 import Contact from "./pages/Contact/Contact";
 import Resources from "./pages/Resources/Resources";
 
+import { Nav, Navbar } from "react-bootstrap";
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <Router>
-      <ul className="navBar">
-        <li>
-          <NavLink exact to="/" id="home"><img src={require("./dubvelopers-logo.png")} alt="Logo"/></NavLink>
-        </li>
-        <li>
-          <NavLink exact to="/team" className="link" activeClassName>Our Team</NavLink>
-        </li>
-        <li>
-          <NavLink to="/projects" className="link" activeClassName>Projects</NavLink>
-        </li>
-        <li>
-          <NavLink to="/resources" className="link" activeClassName>Resources</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact" className="link" activeClassName>Contact Us</NavLink>
-        </li>
-      </ul>
+      <Navbar className="navBar" collapseOnSelect expand="sm">
+        <Navbar.Brand href="/"><img src={require("./dubvelopers-logo.png")} alt="Logo"/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav>
+            <NavLink to="/team" className="link" activeClassName>Our Team</NavLink>
+            <NavLink to="/projects" className="link" activeClassName>Projects</NavLink>
+            <NavLink to="/resources" className="link" activeClassName>Resources</NavLink>
+            <NavLink to="/contact" className="link" activeClassName>Contact Us</NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
         
       <Routes>
         <Route exact path="/" element={<Home/>}/>
